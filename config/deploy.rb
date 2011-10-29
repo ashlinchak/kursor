dc0091ad2c087e1270336f56732c7a69077cc278
ivan <johnny.da.kid@gmail.com>
2011-10-29 15:16:28 +0300

    capified app

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,8 +3,6 @@
 set :using_rvm, true
 $:.unshift(File.expand_path('./lib', ENV['rvm_path']))
 require "rvm/capistrano"
-#set :rvm_ruby_string, 'default'
-#set :rvm_type, :user
 set :rvm_type, :system
 
 # Bundler
@@ -18,9 +16,7 @@ set :user, "root"
 set :port, 3764
 
 set :deploy_to, "/var/www/rails_apps/#{application}"
-#set :deploy_via, :copy
-#set :deploy_via, :remote_cache
-#set :use_sudo, false
+set :deploy_via, :copy
 
 set :normalize_asset_timestamps, false
 
@@ -28,8 +24,8 @@ set :normalize_asset_timestamps, false
 
 set :scm, :git
 set :scm_verbose, true
-#set :repository,  "~/projects/#{application}/.git"
-set :repository,  "git@github.com:ivankukobko/Kursor-Online-catalog.git"
+set :repository,  "~/projects/#{application}/.git"
+#set :repository,  "git@github.com:ivankukobko/Kursor-Online-catalog.git"
 #set :branch, "master"
 
 # VPS
