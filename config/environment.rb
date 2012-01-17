6954f1bf892f425cd150b36e8dc33c01e70c8e71
Ivan Kukobko <johnny.da.kid@gmail.com>
2012-01-17 12:29:02 +0200

    ruby 1.9.2 issues

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+# fix Ruby 1.9.2 encoding issues
+Encoding.default_external = Encoding::UTF_8
+Encoding.default_internal = Encoding::UTF_8
+
 # Initialize the rails application
 Kursor::Application.initialize!
