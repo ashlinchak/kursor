54fe1e162d2a710b6b821d496c6dc1ad94f95fa2
Ivan Kukobko <johnny.da.kid@gmail.com>
2012-01-30 16:42:11 +0200

    home page design tweaks

--- app/models/news.rb
+++ app/models/news.rb
@@ -5,5 +5,5 @@ class News < ActiveRecord::Base
   has_many :images, :as => :imageable, :dependent => :destroy
   accepts_nested_attributes_for :images, :allow_destroy => true, :reject_if => lambda { |i| i[:src].blank? }
 
-  scope :recent, lambda { where('created_at >= ?', Time.now - 1.week).order('created_at desc') }
+  scope :recent, lambda { where('created_at >= ?', Time.now - 1.week).order('created_at desc').limit(5) }
 end
