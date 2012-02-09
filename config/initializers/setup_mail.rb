41989008c5bdb6bfb2c76579348955aa828c1942
MEGAVOLT <megavolt@megavolt-dell>
2012-02-09 17:13:59 +0200

    * SMTP Port changed

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,6 +1,6 @@
 ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
-  :port                 => 587,
+  :port                 => 465,
   :domain               => "kursor.org.ua",
   :user_name            => "noreply@kursor.org.ua",
   :password             => "KuRs0r*pa88*w0rd",
