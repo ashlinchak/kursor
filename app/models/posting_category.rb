b3fde78f7b43c3d66fd76f2ed169ffa6abdca9b8
Alex Ponomarenko <mcmegavolt@gmail.com>
2012-02-22 16:00:26 +0200

    * adding posting categories in admin panel * posting show view file renewed

--- app/models/posting_category.rb
+++ app/models/posting_category.rb
@@ -1,2 +1,14 @@
 class PostingCategory < ActiveRecord::Base
+
+  has_many :posting_categorizings, :dependent => :destroy
+
+  has_many :postings, :through => :posting_categorizings
+
+  validates_presence_of :name, :description #:permalink
+
+  def to_s
+    name
+  end
+
+
 end
