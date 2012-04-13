d761dcb0f072a9509be49f5f761e2e1cd93a8e44
mcmegavolt <mcmegavolt@gmail.com>
2012-04-13 17:52:59 +0300

    grouped city selector

--- app/models/address/city.rb
+++ app/models/address/city.rb
@@ -1,11 +1,15 @@
 class City < ActiveRecord::Base
+
   include Address
+
   def self.table_name_prefix
     'address_'
   end
   belongs_to :region, :class_name => 'Address::Region'
   validates :name, :uniqueness => { :scope => :region_id }
   has_many :locations
+
+
 
   #def to_s
     #name
