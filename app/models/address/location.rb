fe4871f2130d41448cd1832a069a651b638864a6
Ivan Kukobko <johnny.da.kid@gmail.com>
2012-08-14 16:53:31 +0300

    fix googlemaps4rails exception if address/city is missing

--- app/models/address/location.rb
+++ app/models/address/location.rb
@@ -12,9 +12,15 @@ class Location < ActiveRecord::Base
   acts_as_gmappable :msg => I18n.t(:'address.gmaps_error'), :validation => true
 
   def gmaps4rails_address
-    @country = I18n.t(:'address.ukraine')
+    country = I18n.t(:'address.ukraine')
     #"#{@country}, #{self.city.region.name}, #{self.city.name}, #{self.street}, #{self.building}"
-    "#{@country}, #{self.city.name}, #{self.street}, #{self.building}"
+    #"#{@country}, #{self.city.name}, #{self.street}, #{self.building}"
+    addr = [country]
+    addr << city.name if city
+    addr << street unless street.blank?
+    addr << building unless building.blank?
+    addr.compact!
+    return addr.join(' ,')
   end
 
 end
