aa6c16c4585744f54af3a6e30478ad58d8e7ffac
Ivan Kukobko <johnny.da.kid@gmail.com>
2012-08-16 18:33:27 +0300

    fixing nil.categories error

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -43,15 +43,16 @@ class CategoriesController < ApplicationController
 
       providers = []
 
-      addressables.compact.each do |addressable|
-        provider = if addressable.is_a? Filial
-          addressable.provider
-        elsif addressable.is_a? Provider
-          addressable
-        end
-        if provider.categories.include? category or provider.category == category
-          if provider.is_approved
-            providers << provider
+      addressables.each do |addressable|
+        if provider = if addressable.is_a? Filial
+            addressable.provider
+          elsif addressable.is_a? Provider
+            addressable
+          end
+          if provider.categories.include? category or provider.category == category
+            if provider.is_approved
+              providers << provider
+            end
           end
         end
       end
