f87dc270c272d82105bd85e98c9cd4c598b978f3
Ivan Kukobko <johnny.da.kid@gmail.com>
2012-08-22 20:28:02 +0300

    initiated promotions

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,11 +2,7 @@ class Event < ActiveRecord::Base
 
   attr_accessible :description, :end_datetime, :eventable_id, :eventable_type, :location_id, :max_price, :min_price, :start_datetime, :teacher, :title
 
-  #belongs_to :eventable, :polymorphic => true
-
-
-  belongs_to :provider, :polymorphic => true
-  belongs_to :tutor, :polymorphic => true
+  belongs_to :eventable, :polymorphic => true
 
   has_one :location, :as => :addressable, :class_name => 'Address::Location', :dependent => :destroy
   accepts_nested_attributes_for :location
