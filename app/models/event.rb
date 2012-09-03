e26a9871d2ba4d48c7633099da867ce76c3ff791
mcmegavolt <mcmegavolt@gmail.com>
2012-09-03 14:37:16 +0300

    push-push for ivan

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,16 @@
 class Event < ActiveRecord::Base
 
-  attr_accessible :description, :end_datetime, :eventable_id, :eventable_type, :location_id, :max_price, :min_price, :start_datetime, :teacher, :title
+  attr_accessible :description,
+                  :end_datetime,
+                  :eventable_id,
+                  :eventable_type,
+                  :location_id,
+                  :max_price,
+                  :min_price,
+                  :start_datetime,
+                  :teacher,
+                  :title,
+                  :location_attributes
 
   after_initialize :build_nested_resources
 
@@ -10,6 +20,8 @@ class Event < ActiveRecord::Base
   accepts_nested_attributes_for :location
 
   validates_presence_of :start_datetime, :title, :description
+
+  default_scope order('start_datetime ASC')
 
   def build_nested_resources
     self.build_location
