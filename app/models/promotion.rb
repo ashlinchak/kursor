876d40fe0a94f7fbf59d62f32203d087d9e7c259
mcmegavolt <mcmegavolt@gmail.com>
2012-09-12 19:17:42 +0300

    yandex metrika

--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -1,7 +1,7 @@
 class Promotion < ActiveRecord::Base
   belongs_to :promotionable, polymorphic: true
   attr_accessor :provider_name
-  attr_accessible :end_date, :is_featured, :is_vip, :start_date#, :provider_name
+  attr_accessible :end_date, :is_featured, :is_vip, :start_date, :provider_name
   validates :promotionable_id, presence: true
 
   scope :by_position, order('position asc')
@@ -9,6 +9,7 @@ class Promotion < ActiveRecord::Base
 
   def provider_name= name
     if provider = Provider.find_by_name(name)
+      p "provider: #{provider}"
       self.promotionable = provider
     end
   end
