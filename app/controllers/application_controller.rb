98b51bc06d493c6b4612b1c82579df4778f64f83
mcmegavolt <mcmegavolt@gmail.com>
2012-11-07 00:35:48 +0200

    application controlle update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,11 @@ class ApplicationController < ActionController::Base
 
   include SimpleCaptcha::ControllerHelpers
 
-  unless  Rails.env.development?
-    rescue_from ActionController::RedirectBackError,  :with => :render_500
-    rescue_from ActionController::RoutingError,       :with => :render_404
-    rescue_from ActiveRecord::RecordNotFound,         :with => :render_404
-  end
+  #unless  Rails.env.development?
+  #  rescue_from ActionController::RedirectBackError,  :with => :render_500
+  #  rescue_from ActionController::RoutingError,       :with => :render_404
+  #  rescue_from ActiveRecord::RecordNotFound,         :with => :render_404
+  #end
 
   protected
 
