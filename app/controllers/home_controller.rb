6971c759dc44bbeb1f4635ab7aeaa9af821c3c85
mcmegavolt <mcmegavolt@gmail.com>
2012-11-19 19:25:35 +0200

    delayed publishing

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -20,12 +20,12 @@ class HomeController < ApplicationController
   helper_method :providers
 
   def featured_posting
-    @featured_posting ||= Posting.approved.last
+    @featured_posting ||= Posting.published.approved.last
   end
   helper_method :featured_posting
 
   def recent_postings
-    @recent_postings ||= Posting.approved.recent
+    @recent_postings ||= Posting.published.approved.recent
   end
 
   def posting_categories
