85f1eed0c36a4639a5af89433c0a0f00da5164da
mcmegavolt <mcmegavolt@gmail.com>
2013-01-18 14:09:10 +0200

    exclude indicies

---
+++ app/indices/provider_index.rb
@@ -0,0 +1,8 @@
+ThinkingSphinx::Index.define :provider, :with => :active_record do
+  indexes :name
+  indexes :description
+  set_property :field_weights => { :name => 10, :description => 6 }
+  set_property :enable_star => 1
+  set_property :min_infix_len => 3
+  set_property :html_strip => 1
+end
