3fc9a901a3c33ec88b35a9c73621c41847905f32
mcmegavolt <mcmegavolt@gmail.com>
2013-01-18 14:16:47 +0200

    remove indices

--- app/_indices/posting_index.rb
+++
@@ -1,9 +0,0 @@
-ThinkingSphinx::Index.define :posting, :with => :active_record do
-  indexes :title
-  indexes :excerpt
-  indexes :body
-  set_property :field_weights => { :title => 10, :excerpt => 6, :body => 3 }
-  set_property :enable_star => 1
-  set_property :min_infix_len => 3
-  set_property :html_strip => 1
-end
